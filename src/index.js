eba3d6573cbb76f26c1084876401e5182963a7ab
Ramon del Castillo <rdelcast@purdue.edu>
2022-10-13 18:08:56 -0400

    removed firebase errors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import { initializeApp } from 'firebase/app';
 
 // scroll bar
 import 'simplebar/src/simplebar.css';
@@ -12,19 +11,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
 
-//
-const firebaseConfig = {
-  apiKey: "AIzaSyBDY8kkoyGnr5BJOap6e9RZcRJf2xjvT9Q",
-  authDomain: "peerup-431d6.firebaseapp.com",
-  databaseURL: "https://peerup-431d6-default-rtdb.firebaseio.com",
-  projectId: "peerup-431d6",
-  storageBucket: "peerup-431d6.appspot.com",
-  messagingSenderId: "659640437070",
-  appId: "1:659640437070:web:bebb5fbf6f69b2de1af61f",
-  measurementId: "G-8984NE8NQ3"
-};
 
-const app = initializeApp(firebaseConfig);
 
 // ----------------------------------------------------------------------
 
