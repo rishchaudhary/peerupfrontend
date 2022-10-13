d8393436350b2998dc2be9b8bdc99e02dc257e42
Ramon del Castillo <rdelcast@purdue.edu>
2022-10-13 13:20:48 -0400

    Deleted unused files clean up front end repo

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,10 @@ import { Navigate, useRoutes } from 'react-router-dom';
 import DashboardLayout from './layouts/dashboard';
 import LogoOnlyLayout from './layouts/LogoOnlyLayout';
 //
-import Blog from './pages/Blog';
 import User from './pages/User';
 import Login from './pages/Login';
 import NotFound from './pages/Page404';
 import Register from './pages/Register';
-import Products from './pages/Products';
 import Documents from './pages/Document';
 import DashboardApp from './pages/DashboardApp';
 
@@ -23,8 +21,6 @@ export default function Router() {
         { path: 'app', element: <DashboardApp /> },
         { path: 'user', element: <User /> },
         { path: 'document', element: <Documents /> },
-        { path: 'products', element: <Products /> },
-        { path: 'blog', element: <Blog /> },
       ],
     },
     {
