e59de9ac5b8fd59477e837d260b7aebf621c1995
Rish <chaudh36@purdue.edu>
2022-10-13 19:14:14 -0400

    Implemented matches view

--- src/_mock/user.js
+++ src/_mock/user.js
@@ -6,8 +6,11 @@ import { sample } from 'lodash';
 const users = [...Array(24)].map((_, index) => ({
   id: faker.datatype.uuid(),
   name: faker.word.noun(100),
+  tutorName: faker.name.findName(),
+  avatarUrl: `/static/mock-images/avatars/avatar_${index + 1}.jpg`,
   dateuploaded: faker.date.past().toDateString(),
   documenttype: "course content",
+  dateAndTime: faker.date.future().toDateString(),
 }));
 
 export default users;
--- src/pages/DashboardApp.js
+++ src/pages/DashboardApp.js
@@ -1,30 +1,45 @@
-import { faker } from '@faker-js/faker';
-// @mui
-import { useTheme } from '@mui/material/styles';
-import { Grid, Container, Typography } from '@mui/material';
-// tabs 
+// tabs
+import { filter } from 'lodash';
+import { sentenceCase } from 'change-case';
+import { useState } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+// tabs
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
+// material
+import {
+  Card,
+  Table,
+  Stack,
+  Avatar,
+  Button,
+  Checkbox,
+  TableRow,
+  TableBody,
+  TableCell,
+  Container,
+  Typography,
+  TableContainer,
+  TablePagination,
+} from '@mui/material';
+
 // components
 import Page from '../components/Page';
+import Label from '../components/Label';
+import Scrollbar from '../components/Scrollbar';
 import Iconify from '../components/Iconify';
-// sections
-import {
-  AppTasks,
-  AppNewsUpdate,
-  AppOrderTimeline,
-  AppCurrentVisits,
-  AppWebsiteVisits,
-  AppTrafficBySite,
-  AppWidgetSummary,
-  AppCurrentSubject,
-  AppConversionRates,
-} from '../sections/@dashboard/app';
+import SearchNotFound from '../components/SearchNotFound';
+import { UserListHead, UserListToolbar, UserMoreMenu } from '../sections/@dashboard/user';
 
-// ----------------------------------------------------------------------
+
+
+// mock
+import USERLIST from '../_mock/user';
+
+
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
 
@@ -59,20 +74,120 @@ function a11yProps(index) {
 }
 
 
+
+// ----------------------------------------------------------------------
+const TABLE_HEAD = [
+  { id: 'Tutor Name', label: 'Tutor Name', alignRight: false },
+  { id: 'Meeting Time', label: 'Meeting Time', alignRight: false },
+  { id: 'Location', label: 'Location', alignRight: false },
+  { id: 'Rate($/hr)', label: 'Rate($/hr)', alignRight: false },
+  { id: 'Sessions Hosted', label: 'Sessions Hosted', alignRight: false },
+];
+
+// ----------------------------------------------------------------------
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function applySortFilter(array, comparator, query) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  if (query) {
+    return filter(array, (_user) => _user.name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+  }
+  return stabilizedThis.map((el) => el[0]);
+}
+
 export default function DashboardApp() {
-  const theme = useTheme();
+  const [page, setPage] = useState(0);
+
+  const [order, setOrder] = useState('asc');
+
+  const [selected, setSelected] = useState([]);
+
+  const [orderBy, setOrderBy] = useState('name');
+
+  const [filterName, setFilterName] = useState('');
+
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  // tabs
   const [value, setValue] = React.useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      const newSelecteds = USERLIST.map((n) => n.name);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected = [];
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(selected.slice(0, selectedIndex), selected.slice(selectedIndex + 1));
+    }
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleFilterByName = (event) => {
+    setFilterName(event.target.value);
+  };
+
+  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - USERLIST.length) : 0;
+
+  const filteredUsers = applySortFilter(USERLIST, getComparator(order, orderBy), filterName);
+
+  const isUserNotFound = filteredUsers.length === 0;
 
   return (
-    <Page title="Dashboard">
-      <Container maxWidth="xl">
-    
-        <Box sx={{ width: '100%' }}>
+    <Page title="User">
+
+<Box sx={{ width: '100%' }}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <Tabs value={value} onChange={handleChange} aria-label="basic tabs example" centered>
           <Tab label="Matched" {...a11yProps(0)} />
@@ -81,30 +196,97 @@ export default function DashboardApp() {
         </Tabs>
       </Box>
       <TabPanel value={value} index={0}>
-     
-      <Typography variant="h4" sx={{ mb: 5 }}>
-          Matched Sessions
-        </Typography>
+      <Container>
+        <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+          <Typography variant="h4" gutterBottom>
+            Matched
+          </Typography>
+         
+        </Stack>
 
-       
+        <Card>
+         
+          <Scrollbar>
+            <TableContainer sx={{ minWidth: 800 }}>
+              <Table>
+                <UserListHead
+                  order={order}
+                  orderBy={orderBy}
+                  headLabel={TABLE_HEAD}
+                  rowCount={USERLIST.length}
+                  numSelected={selected.length}
+                  onRequestSort={handleRequestSort}
+                  onSelectAllClick={handleSelectAllClick}
+                />
+                <TableBody>
+                  {filteredUsers.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+                    const { id, tutorName, dateuploaded , avatarUrl, dateAndTime} = row;
+                    const isItemSelected = selected.indexOf(tutorName) !== -1;
 
+                    return (
+                      <TableRow
+                        hover
+                        key={id}
+                        tabIndex={-1}
+                        role="checkbox"
+                        selected={isItemSelected}
+                        aria-checked={isItemSelected}>
+                        <TableCell padding="checkbox"/>
+                        <TableCell component="th" scope="row" padding="none">
+                          <Stack direction="row" alignItems="center" spacing={2}>
+                            <Avatar alt={tutorName} src={avatarUrl} />
+                            <Typography variant="subtitle2" noWrap>
+                              {tutorName}
+                            </Typography>
+                          </Stack>
+                        </TableCell>
+                        <TableCell align="left">{dateAndTime}</TableCell>
+                        <TableCell align="left">{dateuploaded}</TableCell>
+                       
 
+                      </TableRow>
+                    );
+                  })}
+                  {emptyRows > 0 && (
+                    <TableRow style={{ height: 53 * emptyRows }}>
+                      <TableCell colSpan={6} />
+                    </TableRow>
+                  )}
+                </TableBody>
 
+                {isUserNotFound && (
+                  <TableBody>
+                    <TableRow>
+                      <TableCell align="center" colSpan={6} sx={{ py: 3 }}>
+                        <SearchNotFound searchQuery={filterName} />
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                )}
+              </Table>
+            </TableContainer>
+          </Scrollbar>
+
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25]}
+            component="div"
+            count={USERLIST.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </Card>
+      </Container>
       </TabPanel>
       <TabPanel value={value} index={1}>
-      <Typography variant="h4" sx={{ mb: 5 }}>
-          Scheduled Sessions
-        </Typography>
+        Item Two
       </TabPanel>
       <TabPanel value={value} index={2}>
-      <Typography variant="h4" sx={{ mb: 5 }}>
-          Completed Sessions
-        </Typography>
+        Item Three
       </TabPanel>
     </Box>
-        
-        
-      </Container>
+      
     </Page>
   );
 }
