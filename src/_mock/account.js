eef9662578855e75593db4be1aac6c7d120e751d
Ramon del Castillo <rdelcast@purdue.edu>
2022-10-13 17:16:56 -0400

    Added more to the profile page

--- src/_mock/account.js
+++ src/_mock/account.js
@@ -2,8 +2,12 @@
 
 const account = {
   displayName: 'Purdue Pete',
+  isStudent: true,
+  major: 'Computer Science',
+  year: 'Sophomore',
   email: 'pete@purdue.edu',
   photoURL: '/static/purdue-pete.png',
+  ratingVal: 4.5,
 };
 
 export default account;
