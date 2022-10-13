b2e2acf367a92345872b7250122380448c84efb3
Rish <chaudh36@purdue.edu>
2022-10-13 15:43:32 -0400

    Adjusted document component

--- src/_mock/user.js
+++ src/_mock/user.js
@@ -7,6 +7,7 @@ const users = [...Array(24)].map((_, index) => ({
   id: faker.datatype.uuid(),
   name: faker.word.noun(100),
   dateuploaded: faker.date.past().toDateString(),
+  documenttype: "course content",
 }));
 
 export default users;
