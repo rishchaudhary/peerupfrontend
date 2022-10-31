0f8b27e92713cd2a5869cf5d9e37c087d652e895
AviNKatare <70462687+AviNKatare@users.noreply.github.com>
2022-10-31 15:08:17 -0400

    Update Tutor.js
    I have commented the file as requested

--- src/Controller/Tutor.js
+++ src/Controller/Tutor.js
@@ -4,6 +4,9 @@ import { Review } from "./Review";
 export class Tutor {
     
 
+    // This function saves the data of a new tutor account to the database.
+    // It sets the value of the HasTutorAccount field of the User class to true. This tells us that the given user is also a tutor.
+    // We will use the user ID as our tutor ID.
     static create_profile(userID, price, courses) {
 
         set(ref(getDatabase(), `Users/${userID}/HasTutorAccount`), true);
@@ -30,7 +33,8 @@ export class Tutor {
 
     }
 
-    // needs to be edited
+    // This function deletes a particular tutor's data from the database.
+    // It sets the HasTutorAccount to false. Meaning that the given user does not have a tutor account
     static delete_profile(userID) {
         
         remove(ref(getDatabase(), `TutorAccounts/${userID}`))
@@ -46,8 +50,11 @@ export class Tutor {
 
     }
 
+    // If the tutor wants to change the list of courses they want to tutor for, they will give us the new list of
+    // courses they want to tutor for. I will then check to see if any of these courses have already been verified by the admin.
+    // if yes, then add them to the list called verified courses else add to the list called notVerified. These two lists are 
+    // set as the value of the keys VerifiedCourses and NotverifiedCourses respectively.
     static async update_courses(userID, courses) {
-        
         
         const tutorData = this.get_information(userID);
         const data = await tutorData.then(val => {return val;});
@@ -75,6 +82,7 @@ export class Tutor {
             }
         }
 
+        // These two if statements are for the purpose of mainting the size of instances across all tutor accounts
         if (values1.length === 0) {
             values1.push('N/A');
         }
@@ -89,9 +97,11 @@ export class Tutor {
         set(ref(getDatabase(), `TutorAccounts/${userID}/NotVerifiedCourses`), values2);
     }
 
-    
+    // Once the admin looks over the tutor's transcript, they must enter a list of verified and not verified courses for
+    // that tutor. This list will then be used by this function to make these changes appear in the database.
     static update_by_admin(userID, newVerifiedList, newNotVerifiedList) {
          
+         // These two if statements are for the purpose of mainting the size of instances across all tutor accounts
         if (newNotVerifiedList.length === 0) {
             newNotVerifiedList.push('N/A');
         }
@@ -105,12 +115,14 @@ export class Tutor {
 
     }
 
+    // This updates the rate of the tutor.
     static update_price(userID, newPrice) {
         
         set(ref(getDatabase(), `TutorAccounts/${userID}/Price`), newPrice);
 
     }
     
+    // This updates the bio of the tutor.
     static update_bio(userID, bio) {
         
         
@@ -118,18 +130,29 @@ export class Tutor {
 
     }
 
+    // This updates the preferred days of the tutor. You guys will create a button for each value in the 
+    // PreferredDays array. Once the button is pressed, the value at that location is changed (if false then set to true and viseversa).
+    // This new value is the UpdatedValue parameter of the function and the Index parameter is the array location that has been changed.
+    // I use them both to make the required changes in the database.
     static update_preferred_days(userID, updatedValue, index) {
 
         set(ref(getDatabase(), `TutorAccounts/${userID}/PreferredDays/${index}`), updatedValue);
         
     }
 
+    // This updates the preferred timings of the tutor. You guys will create a button for each value in the 
+    // PreferredTimings array. Once the button is pressed, the value at that location is changed (if false then set to true and viseversa).
+    // This new value is the UpdatedValue parameter of the function and the Index parameter is the array location that has been changed.
+    // I use them both to make the required changes in the database.
     static update_preferred_times(userID, updatedValue, index) {
 
         set(ref(getDatabase(), `TutorAccounts/${userID}/PreferredTimings/${index}`), updatedValue);
         
     }
 
+    // This function calculates the rating for the given tutor. Called everytime a review is created for that tutor.
+    // If the number of reviews for a given tutor is less than 11, set the rating as 0. If equal to 11, then get the
+    // rating from each review and find the average. If greater than 11, (current rating for tutor + rating from new review) / 2
     static async tutor_rating(userID) {
         
         
@@ -145,7 +168,7 @@ export class Tutor {
             const info = await reviewData.then(val => {return val;});
             rating += parseFloat(info.Rating);
             rating /= 2;
-            set(ref(getDatabase(), `TutorAccounts/${userID}/Rating`), rating / (result.length - 1));
+            set(ref(getDatabase(), `TutorAccounts/${userID}/Rating`), rating);
             
         }
 
@@ -166,11 +189,12 @@ export class Tutor {
         }
 
         else {
-            console.log("Not enough reviews")
+            set(ref(getDatabase(), `TutorAccounts/${userID}/Rating`), 0);
         }
     }
 
 
+    // This gets the promise that contains the information about a particular tutor account.
     static async get_information(userID) {
 
         const db = getDatabase();
