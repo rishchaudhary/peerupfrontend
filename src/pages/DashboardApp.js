536f36da9b94275a8fe75b19354066904a648e8b
Rish <chaudh36@purdue.edu>
2022-11-02 19:40:46 -0400

    Components Added

--- src/pages/DashboardApp.js
+++ src/pages/DashboardApp.js
@@ -1,20 +1,11 @@
+import * as React from 'react';
 // tabs
 import { filter } from 'lodash';
-import { sentenceCase } from 'change-case';
 import { useState } from 'react';
-import { Link as RouterLink } from 'react-router-dom';
-// tabs
-import * as React from 'react';
 import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
-// ratings
-import Rating from '@mui/material/Rating';
-// button
-import IconButton from '@mui/material/IconButton';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import CancelIcon from '@mui/icons-material/Cancel';
 
 // material
 import {
@@ -45,14 +36,10 @@ import MatchedView from '../components/StudentMatched';
 import CompletedView from '../components/StudentCompleted';
 import ScheduledView from '../components/StudentScheduled';
 import Page from '../components/Page';
-import Scrollbar from '../components/Scrollbar';
-import Iconify from '../components/Iconify';
-import SearchNotFound from '../components/SearchNotFound';
-import { UserListHead, UserListToolbar, UserMoreMenu } from '../sections/@dashboard/user';
+
 
 // mock
 import USERLIST from '../_mock/user';
-import StudentCompleted from '../components/StudentCompleted';
 
 
 const auth = getAuth();
@@ -91,50 +78,6 @@ function a11yProps(index) {
 }
 
 
-
-// ----------------------------------------------------------------------
-const TABLE_HEAD = [
-  { id: 'Tutor Name', label: 'Tutor Name', alignRight: false },
-  { id: 'Ratings', label: '', alignRight: false },
-  { id: 'Meeting Time', label: 'Meeting Time', alignRight: false },
-  { id: 'Location', label: 'Location', alignRight: false },
-  { id: 'Rate($/hr)', label: 'Rate($/hr)', alignRight: false },
-  { id: 'Sessions', label: 'Sessions', alignRight: false },
-  { id: 'Accept', label: '', alignRight: false },
-  { id: 'Reject', label: '', alignRight: false },
-];
-
-// ----------------------------------------------------------------------
-
-function descendingComparator(a, b, orderBy) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-function getComparator(order, orderBy) {
-  return order === 'desc'
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-function applySortFilter(array, comparator, query) {
-  const stabilizedThis = array.map((el, index) => [el, index]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) return order;
-    return a[1] - b[1];
-  });
-  if (query) {
-    return filter(array, (_user) => _user.name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
-  }
-  return stabilizedThis.map((el) => el[0]);
-}
-
 async function printUserData() {
   // test2 is the id, pass in currently logged in userid
   const userId = auth.currentUser.uid;
@@ -153,83 +96,12 @@ async function printUserData() {
 
 export default function DashboardApp() {
 
-  printUserData();
-
-  const [page, setPage] = useState(0);
-
-  const [order, setOrder] = useState('asc');
-
-  const [selected, setSelected] = useState([]);
-
-  const [orderBy, setOrderBy] = useState('name');
-
-  const [filterName, setFilterName] = useState('');
-
-  const [rowsPerPage, setRowsPerPage] = useState(5);
-
-
-  const [formValue, setFormValue] = React.useState('Controlled');
-
-  const handleChangeForm = (event) => {
-    setFormValue(event.target.formValue);
-  };
-
   // tabs
   const [value, setValue] = React.useState(1);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
-
-  const handleRequestSort = (event, property) => {
-    const isAsc = orderBy === property && order === 'asc';
-    setOrder(isAsc ? 'desc' : 'asc');
-    setOrderBy(property);
-  };
-
-  const handleSelectAllClick = (event) => {
-    if (event.target.checked) {
-      const newSelecteds = USERLIST.map((n) => n.name);
-      setSelected(newSelecteds);
-      return;
-    }
-    setSelected([]);
-  };
-
-  const handleClick = (event, name) => {
-    const selectedIndex = selected.indexOf(name);
-    let newSelected = [];
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(selected.slice(0, selectedIndex), selected.slice(selectedIndex + 1));
-    }
-    setSelected(newSelected);
-  };
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-
-  const handleFilterByName = (event) => {
-    setFilterName(event.target.value);
-  };
-
-  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - USERLIST.length) : 0;
-
-  const filteredUsers = applySortFilter(USERLIST, getComparator(order, orderBy), filterName);
-
-  const isUserNotFound = filteredUsers.length === 0;
-
 
   // ----------------------------------------------------------------------------------
   return (
@@ -291,8 +163,8 @@ export default function DashboardApp() {
 
             </Stack>
 
-          <StudentCompleted/>
-            
+          <CompletedView/>
+
           </Container>
         </TabPanel>
       </Box>
