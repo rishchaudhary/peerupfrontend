fac2b0730b421feca1ba1ab0662cbf341b7b006a
Rish <chaudh36@purdue.edu>
2022-11-03 13:33:35 -0400

    Added admin user table

--- src/pages/TestAdmin.js
+++ src/pages/TestAdmin.js
@@ -1,4 +1,6 @@
 import { useNavigate } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
 // material
 import {
   Avatar,
@@ -13,6 +15,8 @@ import {
   TextField,
 } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
+
+
 
 // Firebase
 
@@ -36,6 +40,40 @@ import { User as USER } from '../Controller/User';
 
 const auth = getAuth();
 const database = getDatabase();
+
+const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+      field: 'firstName',
+      headerName: 'First name',
+      width: 150,
+      editable: false,
+    },
+    {
+      field: 'lastName',
+      headerName: 'Last name',
+      width: 150,
+      editable: false,
+    },
+    {
+      field: 'emailAddress',
+      headerName: 'Email Address',
+      width: 150,
+      editable: false,
+    },
+  ];
+  
+  const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', emailAddress:'Jon.Snow@purdue.edu' },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei',emailAddress:'Lannister.Cersei@purdue.edu' },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', emailAddress:'Lannister.Jaime@purdue.edu'  },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', emailAddress:'Arya.Stark@purdue.edu'  },
+    {id: 5, lastName: 'Targaryen', firstName: 'Daenerys', emailAddress:'Daenerys.Targaryen@purdue.edu'  },
+    { id: 6, lastName: 'Melisandre', firstName: null, emailAddress:'Melisandre@purdue.edu' },
+    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', emailAddress:'Ferrara.Clifford@purdue.edu'  },
+    { id: 8, lastName: 'Frances', firstName: 'Rossini', emailAddress:'Rossini.Frances@purdue.edu' },
+    { id: 9, lastName: 'Roxie', firstName: 'Harvey', emailAddress:'Harvey.Roxie@purdue.edu'  },
+  ];
 
 export default function AdminPanel() {
     const [inputUid, setInputUid ] = useState();
@@ -75,8 +113,21 @@ export default function AdminPanel() {
             }} >
                 Delete User
             </LoadingButton>
+
+
           </Stack>
         </Stack>
+        <Box sx={{ height: 500, width: '100%' }}>
+                            <DataGrid
+                                rows={rows}
+                                columns={columns}
+                                pageSize={20}
+                                rowsPerPageOptions={[5]}
+                                checkboxSelection
+                                disableSelectionOnClick
+                                experimentalFeatures={{ newEditingApi: true }}
+                            />
+                        </Box>
       </Container>
     </Page>
     )
