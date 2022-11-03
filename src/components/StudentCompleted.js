bd3f0b669a12c87cdc68eb2d81d2023f30daf9b6
Rish <chaudh36@purdue.edu>
2022-11-03 19:16:55 -0400

    StudentCompleted component completed

--- src/components/StudentCompleted.js
+++ src/components/StudentCompleted.js
@@ -9,6 +9,8 @@ import IconButton from '@mui/material/IconButton';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import CancelIcon from '@mui/icons-material/Cancel';
 import { filter } from 'lodash';
+import { DataGrid } from '@mui/x-data-grid';
+import Box from '@mui/material/Box';
 
 // material
 import {
@@ -27,6 +29,9 @@ import {
   TablePagination,
 } from '@mui/material';
 
+// User data 
+import { getAuth } from 'firebase/auth';
+import {getDatabase, ref, onValue} from "firebase/database";
 
 // components
 import Scrollbar from './Scrollbar';
@@ -37,174 +42,91 @@ import { UserListHead, UserListToolbar, UserMoreMenu } from '../sections/@dashbo
 import USERLIST from '../_mock/user';
 
 
+
+const columns = [
+  { field: 'id', headerName: 'ID', width: 100 },
+  {
+      field: 'Tutor',
+      headerName: 'Tutor Name',
+      width: 200,
+      editable: false,
+  },
+  {
+    field: 'Date',
+    headerName: 'Meeting Day',
+    width: 200,
+    editable: false,
+},
+  {
+      field: 'StartTime',
+      headerName: 'Meeting Time',
+      width: 200,
+      editable: false,
+  },
+  {
+    field: 'Location',
+    headerName: 'Location',
+    width: 200,
+    editable: false,
+},
+{
+  field: 'Format',
+  headerName: 'Meeting Format',
+  width: 200,
+  editable: false,
+},
+ 
+];
+
+
+
 export default function StudentCompleted() {
 
-    const [page, setPage] = useState(0);
+  const auth = getAuth();
+const database = getDatabase();
+const userID = getAuth().currentUser.uid;
 
-    const [order, setOrder] = useState('asc');
-  
-    const [selected, setSelected] = useState([]);
-  
-    const [orderBy, setOrderBy] = useState('name');
-  
-    const [filterName, setFilterName] = useState('');
-  
-    const [rowsPerPage, setRowsPerPage] = useState(5);
-  
-  
-    const TABLE_HEAD = [
-      { id: 'Tutor Name', label: 'Tutor Name', alignRight: false },
-      { id: 'Ratings', label: '', alignRight: false },
-      { id: 'Meeting Time', label: 'Meeting Time', alignRight: false },
-      { id: 'Location', label: 'Location', alignRight: false },
-      { id: 'Rate($/hr)', label: 'Rate($/hr)', alignRight: false },
-      { id: 'Sessions', label: 'Sessions', alignRight: false },
-      { id: 'Accept', label: '', alignRight: false },
-      { id: 'Reject', label: '', alignRight: false },
-    ];
-  
-  
-  
-    const handleRequestSort = (event, property) => {
-      const isAsc = orderBy === property && order === 'asc';
-      setOrder(isAsc ? 'desc' : 'asc');
-      setOrderBy(property);
-    };
-  
-    const handleSelectAllClick = (event) => {
-      if (event.target.checked) {
-        const newSelecteds = USERLIST.map((n) => n.name);
-        setSelected(newSelecteds);
-        return;
-      }
-      setSelected([]);
-    };
-  
-    function applySortFilter(array, comparator, query) {
-      const stabilizedThis = array.map((el, index) => [el, index]);
-      stabilizedThis.sort((a, b) => {
-        const order = comparator(a[0], b[0]);
-        if (order !== 0) return order;
-        return a[1] - b[1];
-      });
-      if (query) {
-        return filter(array, (_user) => _user.name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
-      }
-      return stabilizedThis.map((el) => el[0]);
+console.log(userID);
+
+let userSesIDs = [];
+const sessionsIdsRef = ref(database, `Users/${userID}/Sessions`);
+onValue(sessionsIdsRef, (snapshot) => {
+  userSesIDs = snapshot.val();
+});
+
+console.log("Completed Sessions IDS: ", userSesIDs);
+
+const userSesObjs = [];
+for(let i = 1; i < userSesIDs.length; i+= 1){
+  const sessionID = userSesIDs[i];
+  const sesRef = ref(database, `Sessions/${sessionID}`);
+  onValue(sesRef, (snapshot) => {
+    if(snapshot.toJSON().Completed){
+      userSesObjs.push(snapshot.val());
     }
-  
-    function descendingComparator(a, b, orderBy) {
-      if (b[orderBy] < a[orderBy]) {
-        return -1;
-      }
-      if (b[orderBy] > a[orderBy]) {
-        return 1;
-      }
-      return 0;
-    }
-  
-    function getComparator(order, orderBy) {
-      return order === 'desc'
-        ? (a, b) => descendingComparator(a, b, orderBy)
-        : (a, b) => -descendingComparator(a, b, orderBy);
-    }
-  
-    const handleChangePage = (event, newPage) => {
-      setPage(newPage);
-    };
-  
-    const handleChangeRowsPerPage = (event) => {
-      setRowsPerPage(parseInt(event.target.value, 10));
-      setPage(0);
-    };
-  
-    const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - USERLIST.length) : 0;
-  
-    const filteredUsers = applySortFilter(USERLIST, getComparator(order, orderBy), filterName);
-  
-    const isUserNotFound = filteredUsers.length === 0;
+ 
+  });
+}
+console.log("Completed User Session Objects", userSesObjs);
 
 
     return (
-
-        <Card>
-
-              <Scrollbar>
-                <TableContainer sx={{ minWidth: 800 }}>
-                  <Table>
-                    <UserListHead
-                      order={order}
-                      orderBy={orderBy}
-                      headLabel={TABLE_HEAD}
-                      rowCount={USERLIST.length}
-                      numSelected={selected.length}
-                      onRequestSort={handleRequestSort}
-                      onSelectAllClick={handleSelectAllClick}
-                    />
-                    <TableBody>
-                      {filteredUsers.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-                        const { id, tutorName, dateuploaded, avatarUrl, dateAndTime, location, rate, sessionsHosted, rating } = row;
-                        const isItemSelected = selected.indexOf(tutorName) !== -1;
-
-                        return (
-                          <TableRow
-                            hover
-                            key={id}
-                            tabIndex={-1}
-                            selected={isItemSelected}
-                            aria-checked={isItemSelected}>
-                            <TableCell padding="checkbox" />
-                            <TableCell component="th" scope="row" padding="none">
-                              <Stack direction="row" alignItems="center" spacing={2}>
-                                <Avatar alt={tutorName} src={avatarUrl} />
-                                <Typography variant="subtitle2" noWrap>
-                                  {tutorName}
-
-                                </Typography>
-                              </Stack>
-                            </TableCell>
-                            <TableCell align="left"> <Rating name="read-only" value={rating} readOnly /></TableCell>
-                            <TableCell align="left">{dateAndTime}</TableCell>
-                            <TableCell align="left">{location}</TableCell>
-                            <TableCell align="left">{rate}</TableCell>
-                            <TableCell align="left">{sessionsHosted}</TableCell>
-
-                          </TableRow>
-                        );
-                      })}
-                      {emptyRows > 0 && (
-                        <TableRow style={{ height: 53 * emptyRows }}>
-                          <TableCell colSpan={6} />
-                        </TableRow>
-                      )}
-                    </TableBody>
-
-                    {isUserNotFound && (
-                      <TableBody>
-                        <TableRow>
-                          <TableCell align="center" colSpan={6} sx={{ py: 3 }}>
-                            <SearchNotFound searchQuery={filterName} />
-                          </TableCell>
-                        </TableRow>
-                      </TableBody>
-                    )}
-                  </Table>
-                </TableContainer>
-              </Scrollbar>
-
-              <TablePagination
-                rowsPerPageOptions={[5, 10, 25]}
-                component="div"
-                count={USERLIST.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-              />
-            </Card>
+      <Card>
+      <Box sx={{ height: 500, width: '100%' }}>
+      <DataGrid
+          rows={userSesObjs}
+          columns={columns}
+          pageSize={20}
+          rowsPerPageOptions={[5]}
+          disableSelectionOnClick
+          experimentalFeatures={{ newEditingApi: true }}
+      />
+  </Box>
+  </Card>
 
 
     );
+
 
 
 }
