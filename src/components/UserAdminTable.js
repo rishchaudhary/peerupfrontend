ee6e644fdc02a94c6090b22b406bcd208cdbdae4
Rish <chaudh36@purdue.edu>
2022-11-03 23:01:07 -0400

    UserAdminTable changes

--- src/components/UserAdminTable.js
+++ src/components/UserAdminTable.js
@@ -9,81 +9,59 @@ import {getAuth} from "firebase/auth";
 const columns = [
     { field: 'id', headerName: 'ID', width: 200 },
     {
-        field: 'firstName',
-        headerName: 'First name',
+        field: 'Name',
+        headerName: 'Name',
         width: 200,
         editable: false,
     },
     {
-        field: 'lastName',
-        headerName: 'Last name',
+        field: 'Email',
+        headerName: 'Email',
         width: 200,
         editable: false,
     },
     {
-        field: 'emailAddress',
-        headerName: 'Email Address',
+        field: 'UserID',
+        headerName: 'UserID',
         width: 200,
         editable: false,
     },
 ];
 
-const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', emailAddress:'Jon.Snow@purdue.edu' },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei',emailAddress:'Lannister.Cersei@purdue.edu' },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', emailAddress:'Lannister.Jaime@purdue.edu'  },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', emailAddress:'Arya.Stark@purdue.edu'  },
-    {id: 5, lastName: 'Targaryen', firstName: 'Daenerys', emailAddress:'Daenerys.Targaryen@purdue.edu'  },
-    { id: 6, lastName: 'Melisandre', firstName: null, emailAddress:'Melisandre@purdue.edu' },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', emailAddress:'Ferrara.Clifford@purdue.edu'  },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', emailAddress:'Rossini.Frances@purdue.edu' },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', emailAddress:'Harvey.Roxie@purdue.edu'  },
-
-  ];
 
 
 export default function UserAdminTable() {
 
+    const auth = getAuth();
     const database = getDatabase();
-
-    let userObjs = [];
-    const userRef = ref(database, `Users`);
-    onValue(userRef, (snapshot) => {
-        userObjs = snapshot.val();
-        
-    })
-
-    // console.log(userObjs.value.key);
-
-    // console.log("User lists", userObjs);    
-
-    /*
-   const usersList = [];
-   for(let i = 0; i < userObjs.length; i+= 1){
-        if(userObjs[i].value){
-         usersList.push(i);
-        }
-        console.log(usersList[i]);
-   }
-   */
-
-    const data = [
-        { id: 1, lastName: 'Snow', firstName: 'Rish', emailAddress:'Jon.Snow@purdue.edu' },
-        { id: 2, lastName: 'Lannister', firstName: 'Cersei',emailAddress:'Lannister.Cersei@purdue.edu' },
-        { id: 3, lastName: 'Lannister', firstName: 'Jaime', emailAddress:'Lannister.Jaime@purdue.edu'  },
-        { id: 4, lastName: 'Stark', firstName: 'Arya', emailAddress:'Arya.Stark@purdue.edu'  },
-        {id: 5, lastName: 'Targaryen', firstName: 'Daenerys', emailAddress:'Daenerys.Targaryen@purdue.edu'  },
-        { id: 6, lastName: 'Melisandre', firstName: null, emailAddress:'Melisandre@purdue.edu' },
-        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', emailAddress:'Ferrara.Clifford@purdue.edu'  },
-        { id: 8, lastName: 'Frances', firstName: 'Rossini', emailAddress:'Rossini.Frances@purdue.edu' },
-        { id: 9, lastName: 'Roxie', firstName: 'Harvey', emailAddress:'Harvey.Roxie@purdue.edu'  },
+    const userID = getAuth().currentUser.uid;
     
-      ];
+    console.log(userID);
+    
+    let userIDs = [];
+    const userIdsRef = ref(database, `Users`);
+    onValue(userIdsRef, (snapshot) => {
+      userIDs = snapshot.val();
+    });
+
+    console.log("Users:", userIDs);
+
+   const userObjs = [];
+   for(let i = 1; i < userIDs.length; i+= 1){
+        const userID = userIDs[i];
+        const userRef = ref(database, `Users/${userID}`);
+        onValue(userRef, (snapshot) =>{
+            userObjs.push(snapshot.val());
+        });
+   }
+
+    console.log(userIDs);
+   
 
   return (
     <Box sx={{ height: 500, width: '100%' }}>
     <DataGrid
-        rows={data}
+        rows={userObjs}
         columns={columns}
         pageSize={20}
         rowsPerPageOptions={[5]}
