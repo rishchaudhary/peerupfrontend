315245473cb7c6d6b967552d8733f05cad0a9a96
AviNKatare <70462687+AviNKatare@users.noreply.github.com>
2022-11-23 19:04:22 -0500

    Update Feedback.js
    Made changes to the delete_feedback(). Now if you pass it an array of feedbackIDs it can delete them all at once.

--- src/Controller/Feedback.js
+++ src/Controller/Feedback.js
@@ -46,47 +46,52 @@ export class Feedback {
         set(ref(getDatabase(), `Feedback/${feedbackID}/WhyDisputed`), comment);
     }
 
-    static async delete_feedback(feedbackID) {
+    static async delete_feedback(feedbackIDs) {
 
-        const feedbackUserID = feedbackID.split('/');
-        const reviewData = this.getFeedbackInformation(feedbackID);
-        const data = await reviewData.then(val => {
-            return val;
-        });
-        const tutor = data.CreatedBy;
-        const user = data.CreatedFor;
+        /* eslint-disable no-await-in-loop */
+        for (let j = 0; j < feedbackIDs.length; j += 1) {
+            const feedbackUserID = feedbackIDs[j].split('/');
+            const reviewData = this.getFeedbackInformation(feedbackIDs[j]);
+            const data = await reviewData.then(val => {
+                return val;
+            });
 
-        const userData = User.get_information(user);
-        const data2 = await userData.then(val => {
-            return val;
-        });
-        const reviews = data2.Feedback;
-        let result = Object.keys(reviews).map((key) => reviews[key]);
+            const tutor = data.CreatedBy;
+            const user = data.CreatedFor;
 
-        for (let i = 0; i < result.length; i += 1) {
-            if (feedbackID === result[i]) {
-                result.splice(i, 1);
-                set(ref(getDatabase(), `Users/${user}/Feedback`), result);
-                break;
+            const userData = User.get_information(user);
+            const data2 = await userData.then(val => {
+                return val;
+            });
+            const reviews = data2.Feedback;
+            let result = Object.keys(reviews).map((key) => reviews[key]);
+
+            for (let i = 0; i < result.length; i += 1) {
+                if (feedbackIDs[j] === result[i]) {
+                    result.splice(i, 1);
+                    set(ref(getDatabase(), `Users/${user}/Feedback`), result);
+                    break;
+                }
             }
-        }
 
-        const tutorData = Tutor.get_information(tutor);
-        const data3 = await tutorData.then(val => {
-            return val;
-        });
-        const reviewsForTutor = data3.Feedback;
-        result = Object.keys(reviewsForTutor).map((key) => reviewsForTutor[key]);
+            const tutorData = Tutor.get_information(tutor);
+            const data3 = await tutorData.then(val => {
+                return val;
+            });
+            const reviewsForTutor = data3.Feedback;
+            result = Object.keys(reviewsForTutor).map((key) => reviewsForTutor[key]);
 
-        for (let i = 0; i < result.length; i += 1) {
-            if (feedbackUserID[1] === result[i]) {
-                result.splice(i, 1);
-                set(ref(getDatabase(), `TutorAccounts/${tutor}/Feedback`), result);
-                break;
+            for (let i = 0; i < result.length; i += 1) {
+                if (feedbackUserID[1] === result[i]) {
+                    result.splice(i, 1);
+                    set(ref(getDatabase(), `TutorAccounts/${tutor}/Feedback`), result);
+                    break;
+                }
             }
-        }
 
-        set(ref(getDatabase(), `Feedback/${feedbackID}`), null);
+            set(ref(getDatabase(), `Feedback/${feedbackIDs[j]}`), null);
+        }
+        /* eslint-disable no-await-in-loop */
 
     }
 
