a99b0406302fae82903932a37444f5b419499730
AviNKatare <70462687+AviNKatare@users.noreply.github.com>
2022-11-24 18:07:36 -0500

    Update MatchingAlgorithm.js
    The matching algorithm now also matches on criteria like time and day. It matches the tutors based on priorities set by the user when they create a request.

--- src/Controller/MatchingAlgorithm.js
+++ src/Controller/MatchingAlgorithm.js
@@ -6,52 +6,295 @@ import { sendEmailNotification } from "./SendEmail";
 export class MatchingAlgorithm{
 
 
-    static async match(requestID) {
+    static async match(requestID, priorityList) {
 
         const requestData = Requests.get_information(requestID)
         const data = await requestData.then(val => {return val;});
         const language = data.LanguagePreference;
         const course = data.CourseWanted;
 
+        const timeData = data.Time;
+        const timeData2 = timeData.split(' ');
+        const timeData3 = timeData2[4].split(':');
+        const timeData4 = parseFloat(timeData3[0]);
+        let time;
+
+        if (timeData4 < 12.0) {
+            time = 0;
+        }
+        else if (timeData4 > 16) {
+            time = 2;
+        }
+        else {
+            time = 1;
+        }
+
+        const dateData = data.Date.split(',')[0];
+        let day;
+
+        if (dateData === 'Mon') {
+            day = 0
+        }
+        else if (dateData === 'Tue') {
+            day = 1;
+        }
+        else if (dateData === 'Wed') {
+            day = 2;
+        }
+        else if (dateData === 'Thu') {
+            day = 3;
+        }
+        else if (dateData === 'Fri') {
+            day = 4;
+        }
+        else if (dateData === 'Sat') {
+            day = 5;
+        }
+        else {
+            day = 6;
+        }
+
         const tutorListData = this.get_all_tutors();
         const data2 = await tutorListData.then(val => {return val;});
         const tutorIDs = Object.keys(data2);
-        const tutorList = [];
+        let tutorList = [];
 
-        /* eslint-disable no-await-in-loop */
-        for (let i = 0; i < tutorIDs.length; i += 1) {
+        if (priorityList[0] === 'language') {
 
-            const tutorData = Tutor.get_information(tutorIDs[i]);
-            const data3 = await tutorData.then(val => {return val;});
-            const verifiedCourses = data3.VerifiedCourses;
-            const requests = data3.Requests;
-            const verifiedCoursesArray = Object.keys(verifiedCourses).map((key) => verifiedCourses[key]);
-            const requestsArray = Object.keys(requests).map((key) => requests[key]);
-            const languageTutor = data3.Language;
+            /* eslint-disable no-await-in-loop */
+            for (let i = 0; i < tutorIDs.length; i += 1) {
 
-            for (let j = 0; j < verifiedCoursesArray.length; j += 1) {
+                const tutorData = Tutor.get_information(tutorIDs[i]);
+                const data3 = await tutorData.then(val => {return val;});
+                const languageForTutor = data3.Language;
 
-                if ((verifiedCoursesArray[j] === course) && (language === languageTutor)) {
-
+                if(languageForTutor === language) {
                     tutorList.push(tutorIDs[i]);
-                    requestsArray.push(requestID);
-                    set(ref(getDatabase(), `TutorAccounts/${tutorIDs[i]}/Requests`), requestsArray);
+                }
+            }
+            /* eslint-disable no-await-in-loop */
+        }
+
+        else if (priorityList[0] === 'time') {
+
+            /* eslint-disable no-await-in-loop */
+            for (let i = 0; i < tutorIDs.length; i += 1) {
+
+                const tutorData = this.get_specific_time(tutorIDs[i], time);
+                const data3 = await tutorData.then(val => {return val;});
+
+
+                if(data3) {
+                    tutorList.push(tutorIDs[i]);
+                }
+            }
+            /* eslint-disable no-await-in-loop */
+        }
+
+        else if (priorityList[0] === 'day') {
+
+            /* eslint-disable no-await-in-loop */
+            for (let i = 0; i < tutorIDs.length; i += 1) {
+
+                const tutorData = this.get_specific_day(tutorIDs[i], day);
+                const data3 = await tutorData.then(val => {return val;});
+
+
+                if(data3) {
+                    tutorList.push(tutorIDs[i]);
+                }
+            }
+            /* eslint-disable no-await-in-loop */
+        }
+
+        else {
+
+            const listOfVerifiedTutors = [];
+            const listOfNotVerifiedTutors = [];
+
+            /* eslint-disable no-await-in-loop */
+            for (let i = 0; i < tutorIDs.length; i += 1) {
+
+                const tutorData = Tutor.get_information(tutorIDs[i]);
+                const data3 = await tutorData.then(val => {
+                    return val;
+                });
+                const verifiedCourses = data3.VerifiedCourses;
+                const verifiedCoursesArray = Object.keys(verifiedCourses).map((key) => verifiedCourses[key]);
+                const notVerifiedCourses = data3.NotVerifiedCourses;
+                const notVerifiedCoursesArray = Object.keys(notVerifiedCourses).map((key) => notVerifiedCourses[key]);
+
+                for (let j = 0; j < verifiedCoursesArray.length; j += 1) {
+
+                    if (verifiedCoursesArray[j] === course) {
+
+                        listOfVerifiedTutors.push(tutorIDs[i]);
+                    }
+
+                }
+
+                for (let k = 0; k < notVerifiedCoursesArray.length; k += 1) {
+
+                    if (notVerifiedCoursesArray[k] === course) {
+
+                        listOfNotVerifiedTutors.push(tutorIDs[i]);
+                    }
+
+                }
+            }
+            /* eslint-disable no-await-in-loop */
+
+            if (listOfVerifiedTutors.length !== 0) {
+                tutorList = listOfVerifiedTutors;
+            } else if (listOfNotVerifiedTutors.length !== 0) {
+                tutorList = listOfNotVerifiedTutors;
+            } else {
+                tutorList = tutorIDs;
+            }
+        }
+
+        if (tutorList.length === 0) {
+            tutorList = tutorIDs;
+        }
+
+        console.log('yes');
+        console.log(tutorList);
+        for (let m = 1; m < priorityList.length; m += 1) {
+            tutorList = await this.match_2(priorityList[m], tutorList, language, course, time, day);
+        }
+
+
+        console.log(tutorList);
+        /* eslint-disable no-await-in-loop */
+        for (let i = 0; i < tutorList.length; i += 1) {
+
+            const tutorData = Tutor.get_information(tutorList[i]);
+            const data3 = await tutorData.then(val => {return val;});
+            const requests = data3.Requests;
+            const requestsArray = Object.keys(requests).map((key) => requests[key]);
+            requestsArray.push(requestID);
+            await set(ref(getDatabase(), `TutorAccounts/${tutorList[i]}/Requests`), requestsArray);
                     // send email to tutor that they have matched with someone
-                    const tutorEmail = await (await get(ref(getDatabase(), `Users/${tutorIDs[i]}/Email`))).val();
+                    const tutorEmail = await (await get(ref(getDatabase(), `Users/${tutorList[i]}/Email`))).val();
                     console.log("email: ", tutorEmail);
-                    
+
                     sendEmailNotification(tutorEmail).then(() => {
                         console.log("Tutor", tutorEmail[i], "sent email notification")
                     }).catch((error) => {
                         console.log("Error sending email notification: ", error);
                     });
-                }
-
-            }
         }
         /* eslint-disable no-await-in-loop */
 
         return tutorList;
+    }
+
+    static async match_2(priorityItem, tutorIDs, language, course, time, day) {
+
+        let tutorList = [];
+
+        if (priorityItem === 'language') {
+
+            /* eslint-disable no-await-in-loop */
+            for (let i = 0; i < tutorIDs.length; i += 1) {
+
+                const tutorData = Tutor.get_information(tutorIDs[i]);
+                const data3 = await tutorData.then(val => {return val;});
+                const languageForTutor = data3.Language;
+
+                if(languageForTutor === language) {
+                    tutorList.push(tutorIDs[i]);
+                }
+            }
+            /* eslint-disable no-await-in-loop */
+        }
+
+        else if (priorityItem === 'time') {
+
+            /* eslint-disable no-await-in-loop */
+            for (let i = 0; i < tutorIDs.length; i += 1) {
+
+                const tutorData = this.get_specific_time(tutorIDs[i], time);
+                const data3 = await tutorData.then(val => {return val;});
+
+
+                if(data3) {
+                    console.log(data3);
+                    console.log(tutorIDs[i]);
+                    tutorList.push(tutorIDs[i]);
+                }
+            }
+            /* eslint-disable no-await-in-loop */
+        }
+
+        else if (priorityItem === 'day') {
+
+            /* eslint-disable no-await-in-loop */
+            for (let i = 0; i < tutorIDs.length; i += 1) {
+
+                const tutorData = this.get_specific_day(tutorIDs[i], day);
+                const data3 = await tutorData.then(val => {return val;});
+
+
+                if(data3) {
+                    tutorList.push(tutorIDs[i]);
+                }
+            }
+            /* eslint-disable no-await-in-loop */
+        }
+
+        else {
+
+            const listOfVerifiedTutors = [];
+            const listOfNotVerifiedTutors = [];
+
+            /* eslint-disable no-await-in-loop */
+            for (let i = 0; i < tutorIDs.length; i += 1) {
+
+                const tutorData = Tutor.get_information(tutorIDs[i]);
+                const data3 = await tutorData.then(val => {
+                    return val;
+                });
+                const verifiedCourses = data3.VerifiedCourses;
+                const verifiedCoursesArray = Object.keys(verifiedCourses).map((key) => verifiedCourses[key]);
+                const notVerifiedCourses = data3.NotVerifiedCourses;
+                const notVerifiedCoursesArray = Object.keys(notVerifiedCourses).map((key) => notVerifiedCourses[key]);
+
+                for (let j = 0; j < verifiedCoursesArray.length; j += 1) {
+
+                    if (verifiedCoursesArray[j] === course) {
+
+                        listOfVerifiedTutors.push(tutorIDs[i]);
+                    }
+
+                }
+
+                for (let k = 0; k < notVerifiedCoursesArray.length; k += 1) {
+
+                    if (notVerifiedCoursesArray[k] === course) {
+
+                        listOfNotVerifiedTutors.push(tutorIDs[i]);
+                    }
+
+                }
+            }
+            /* eslint-disable no-await-in-loop */
+
+            if (listOfVerifiedTutors.length !== 0) {
+                tutorList = listOfVerifiedTutors;
+            } else if (listOfNotVerifiedTutors.length !== 0) {
+                tutorList = listOfNotVerifiedTutors;
+            } else {
+                tutorList = tutorIDs;
+            }
+        }
+
+        if (tutorList.length === 0) {
+            return tutorIDs;
+        }
+
+        return tutorList;
+
     }
 
 
@@ -60,6 +303,21 @@ export class MatchingAlgorithm{
         const db = getDatabase();
         const tutorRef = ref(db, `TutorAccounts`);
         const snapshot = (await (get(tutorRef))).toJSON();
+        return snapshot;
+    }
+
+    static async get_specific_day(userID, num) {
+        const db = getDatabase();
+        const daysRef = ref(db, `TutorAccounts/${userID}/PreferredDays/${num}/value`);
+        const snapshot = (await (get(daysRef))).toJSON();
+        return snapshot;
+    }
+
+    static async get_specific_time(userID, num) {
+        const db = getDatabase();
+        const timesRef = ref(db, `TutorAccounts/${userID}/PreferredTimings/${num}/value`);
+        const snapshot = (await (get(timesRef))).toJSON();
+        console.log(snapshot)
         return snapshot;
     }
 }
